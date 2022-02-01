66a2927fac4eb72fb2c1378f328dd1e2ddcfd2b5
shun0410 <shun0410@localhost>
2022-02-01 22:50:51 +0900

    [Add]４章確認問題

---
+++ src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "中島俊太郎";
+		System.out.println(str);
+
+		str = "粉骨砕身";
+		System.out.println(str);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
