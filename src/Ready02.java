e47ba2ca9bed9994f281f51832596c6d26313fad
shun0410 <shun0410@localhost>
2022-02-01 22:08:25 +0900

    [Update]コメント追加

--- src/Ready02.java
+++ src/Ready02.java
@@ -1,5 +1,6 @@
 public class Ready02 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		// メソッドの処理を記述
+		System.out.println("Hello World!");
+	}
 }
