512318d0f99d9f567170ffceddcb6cc330546032
shun0410 <shun0410@localhost>
2022-02-02 22:26:56 +0900

    [Add]５章確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,27 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 123456789L;
+		System.out.println(l);
+
+		double d = 1234567.89d;
+		System.out.println(d);
+
+		boolean b = true;
+		System.out.println(b);
+
+		Float f = 0.123f;
+		System.out.println(f);
+
+		Integer i = 5;
+		System.out.println(i);
+
+		Character c = 'う';
+		System.out.println(c);
+
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+
+	}
+
+}
