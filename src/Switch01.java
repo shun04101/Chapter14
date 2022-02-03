498d06d349e7a6593646b85ceb61cb18420ccb57
shun0410 <shun0410@localhost>
2022-02-03 21:08:47 +0900

    [Add]７章カリキュラム

---
+++ src/Switch01.java
@@ -0,0 +1,27 @@
+
+public class Switch01 {
+	public static void main(String[] args) {
+		int val = 2;
+
+		switch (val) {
+		case 3:
+			System.out.println("よく出来ました！");
+			break;
+		case 2:
+			System.out.println("普通でした！");
+			break;
+		default:
+			System.out.println("頑張ろう！");
+		}
+
+		//		int val = 2;
+		//		if (val == 3) {
+		//			System.out.println("よく出来ました！");
+		//		} else if (val == 2) {
+		//			System.out.println("普通でした！");
+		//		} else {
+		//			System.out.println("頑張ろう！");
+		//		}
+	}
+
+}
